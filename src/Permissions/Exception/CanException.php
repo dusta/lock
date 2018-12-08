9b1a3b391c95a665f43e89704b7af758275faa15
Sławomir Kaleta <slaszka@gmail.com>
2018-12-08 19:56:36 +0100

    Resolved #3, Add Exceptions

---
+++ src/Permissions/Exception/CanException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BeatSwitch\Lock\Permissions\Exception;
+
+/**
+ * Class CanException
+ *
+ * @package BeatSwitch\Lock\Permissions\Exception
+ */
+class CanException extends \Exception
+{
+
+}
---
+++ src/Permissions/Exception/CannotException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BeatSwitch\Lock\Permissions\Exception;
+
+/**
+ * Class CannotException
+ *
+ * @package BeatSwitch\Lock\Permissions\Exception
+ */
+class CannotException extends \Exception
+{
+
+}
